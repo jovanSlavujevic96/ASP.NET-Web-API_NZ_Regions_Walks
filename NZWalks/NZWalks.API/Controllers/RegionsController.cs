160b6db9af5e23e1782371c829e0fec0a587a0d2
jovanSlavujevic96 <jotzilla96@gmail.com>
2024-09-20 00:15:11 +0200

    Trying out controller with hardcoded values

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:portnumber/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        // GET ALL REGIONS
+        // GET: https://localhost:portnumber/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new() {
+                    Id = Guid.NewGuid(),
+                    Name = "Auckland Regions",
+                    Code = "AKL",
+                    RegionImageUrl = "https://outthere.kiwi/wp-content/uploads/Auckland-harbour-1024x576.jpg"
+                },
+                new() {
+                    Id = Guid.NewGuid(),
+                    Name = "Wellington Regions",
+                    Code = "WLG",
+                    RegionImageUrl = "https://www.nzonline.org.nz/wp-content/uploads/2024/09/Wellington-city-2000x1334.jpg"
+                }
+            };
+
+            return Ok(regions);
+        }
+    }
+}
