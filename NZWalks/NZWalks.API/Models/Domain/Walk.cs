32f99dab3c94b073d18c9404120963921f440832
jovanSlavujevic96 <jotzilla96@gmail.com>
2024-09-19 23:20:16 +0200

    DbContext

--- NZWalks/NZWalks.API/Models/Domain/Walk.cs
+++ NZWalks/NZWalks.API/Models/Domain/Walk.cs
@@ -2,17 +2,17 @@
 {
     public class Walk
     {
-        public Guid Id{ get; set; }
-        required public string Name{ get; set; }
+        public Guid Id { get; set; }
+        required public string Name { get; set; }
         required public string Description{ get; set; }
         public double LengthInKm { get; set; }
-        public string? WalkImageUrl{ get; set; }
+        public string? WalkImageUrl { get; set; }
 
-        public Guid DifficultyId{ get; set; }
-        public Guid RegionId{ get; set; }
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
 
         // Navigation properties
-        required public Difficulty Difficulty{ get; set; }
+        required public Difficulty Difficulty { get; set; }
         required public Region Region { get; set; }
     }
 }
