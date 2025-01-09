87818c132173fba0fce060ba28e75b5dfa2699d9
jovanSlavujevic96 <jotzilla96@gmail.com>
2025-01-10 00:27:19 +0100

    Implement Repository Pattern

---
+++ NZWalks/NZWalks.API/Repositories/IRegionRepository.cs
@@ -0,0 +1,13 @@
+﻿using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public interface IRegionRepository
+    {
+        Task<List<Region>> GetAllAsync();
+        //Task<Region> GetById(Guid id);
+        //Task<Region> Create(Region region);
+        //Task<Region> Update(Guid id, Region region);
+        //Task<Region> Delete(Guid id);
+    }
+}
