23c7982ffc51fe0d22c85a1849751e40b8d9fe37
jovanSlavujevic96 <jotzilla96@gmail.com>
2025-01-09 23:01:04 +0100

    Create Region Action Method

--- NZWalks/NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -67,7 +67,38 @@ namespace NZWalks.API.Controllers
             };
 
             // Return DTO
-            return Ok(regionDto);
+            return Ok(regionDto); // 200 OK
+        }
+
+        // POST to Create New Region
+        // POST: https://localhost:portnumber/api/regions
+        [HttpPost]
+        public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            // Map/Convert DTO to Domain Model
+            var regionDomainModel = new Region
+            {
+                Id = Guid.NewGuid(),
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+            };
+
+            // Use Domain Model to create Region
+            // Add to Database
+            dbContext.Regions.Add(regionDomainModel);
+            dbContext.SaveChanges(); // ACtually saves changes
+
+            // Map Domain model back to DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+
+            return CreatedAtAction(nameof(GetById), new { id = regionDto.Id }, regionDto); // 201 Created
         }
     }
 }
---
+++ NZWalks/NZWalks.API/Models/DTO/AddRegionRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace NZWalks.API.Models.DTO
+{
+    public class AddRegionRequestDto
+    {
+        required public string Code { get; set; }
+        required public string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+    }
+}
