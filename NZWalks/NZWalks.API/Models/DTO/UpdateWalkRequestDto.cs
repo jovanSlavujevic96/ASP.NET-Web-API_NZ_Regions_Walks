39322b10e40f49f971623375f039097659095486
jovanSlavujevic96 <jotzilla96@gmail.com>
2025-01-10 21:38:03 +0100

    Update Walk Action Method

---
+++ NZWalks/NZWalks.API/Models/DTO/UpdateWalkRequestDto.cs
@@ -0,0 +1,13 @@
+﻿namespace NZWalks.API.Models.DTO
+{
+    public class UpdateWalkRequestDto
+    {
+        required public string Name { get; set; }
+        required public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+    }
+}
