ed7d7fa12ae8d809d6737307cf6607ef4e350210
jovanSlavujevic96 <jotzilla96@gmail.com>
2025-01-11 16:12:08 +0100

    Create Domain Model For Image Uplaod

---
+++ NZWalks/NZWalks.API/Models/Domain/Image.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NZWalks.API.Models.Domain
+{
+    public class Image
+    {
+        required public Guid Id{ get; set; }
+
+        [NotMapped]
+        public IFormFile File { get; set; }
+
+        required public string FileName { get; set; }
+        public string? FileDescription { get; set; }
+        required public string FileExtension { get; set; }
+        required public long FileSizeInBytes { get; set; }
+        required public string FilePath { get; set; }
+    }
+}
