04ae92a5a016925aa7c8ed92c34843f237df800b
jovanSlavujevic96 <jotzilla96@gmail.com>
2025-01-11 01:08:11 +0100

    Create Auth Controller & Register Action Method

---
+++ NZWalks/NZWalks.API/Models/DTO/RegisterRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.API.Models.DTO
+{
+    public class RegisterRequestDto
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        required public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        required public string Password { get; set; }
+
+        public string[] Roles { get; set; }
+    }
+}
