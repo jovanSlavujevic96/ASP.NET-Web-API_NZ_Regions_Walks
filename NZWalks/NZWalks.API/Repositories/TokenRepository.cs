824b46463c2303a4919505060db2f81d2738ad0f
jovanSlavujevic96 <jotzilla96@gmail.com>
2025-01-11 01:50:30 +0100

    Create JWT Token in ASP.NET Core WEB API

---
+++ NZWalks/NZWalks.API/Repositories/TokenRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace NZWalks.API.Repositories
+{
+    public class TokenRepository : ITokenRepository
+    {
+        private readonly IConfiguration configuration;
+
+        public TokenRepository(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string CreateJwtToken(IdentityUser user, List<string> roles)
+        {
+            // Create claims
+            var claims = new List<Claim>();
+
+            if (String.IsNullOrEmpty(user.Email))
+            {
+                throw new ArgumentNullException(nameof(user), "User email cannot be null or empty.");
+            }
+            claims.Add(new Claim(ClaimTypes.Email, value: user.Email));
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, value: role));
+            }
+
+            var jwtKey = configuration["Jwt:Key"];
+            if (string.IsNullOrEmpty(jwtKey))
+            {
+                throw new ArgumentNullException(nameof(jwtKey), "JWT key cannot be null or empty.");
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: configuration["Jwt:Issuer"],
+                audience: configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
