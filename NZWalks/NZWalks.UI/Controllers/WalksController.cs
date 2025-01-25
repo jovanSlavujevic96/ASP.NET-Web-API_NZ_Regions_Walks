c49d2f1dae51201fcc0469792941b91236c05b81
jovanSlavujevic96 <jotzilla96@gmail.com>
2025-01-25 19:03:55 +0100

    DELETE - Deleting a Resource using a Web API

--- NZWalks/NZWalks.UI/Controllers/WalksController.cs
+++ NZWalks/NZWalks.UI/Controllers/WalksController.cs
@@ -125,5 +125,25 @@ namespace NZWalks.UI.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(RegionDto request)
+        {
+            try
+            {
+                var client = httpClientFactory.CreateClient();
+
+                var httpResponseMessage = await client.DeleteAsync("https://localhost:7060" + $"/api/v1/walks/{request.Id}");
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                return RedirectToAction("Index", "Walks");
+            }
+            catch (Exception /*ex*/)
+            {
+                // Log the exception
+            }
+
+            return View("Edit");
+        }
     }
 }
