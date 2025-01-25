afc502cab4054536d0b69c87f6c10acfab901965
jovanSlavujevic96 <jotzilla96@gmail.com>
2025-01-25 18:03:32 +0100

    POST - Consume Web API through UI

--- NZWalks/NZWalks.UI/Controllers/WalksController.cs
+++ NZWalks/NZWalks.UI/Controllers/WalksController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.UI.Models;
 using NZWalks.UI.Models.DTO;
+using System.Text;
+using System.Text.Json;
 
 namespace NZWalks.UI.Controllers
 {
@@ -12,6 +15,7 @@ namespace NZWalks.UI.Controllers
             this.httpClientFactory = httpClientFactory;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             List<WalkDto> response = new List<WalkDto>();
@@ -27,18 +31,48 @@ namespace NZWalks.UI.Controllers
 
                 httpResponseMessage.EnsureSuccessStatusCode();
 
-                var regions = await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<WalkDto>>();
-                if (regions != null)
+                var walks = await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<WalkDto>>();
+                if (walks != null)
                 {
-                    response.AddRange(regions);
+                    response.AddRange(walks);
                 }
 
             }
-            catch (Exception ex)
+            catch (Exception /*ex*/)
             {
                 // Log the exception
             }
             return View(response);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddWalkViewModel model)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var httpRequestMessage = new HttpRequestMessage(){
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://localhost:7060" /* from NZWalks.API sln -> Properties/launchSettings.json -> "https/applicationUrl" key */
+                                     + "/api/v1/walks"),
+                Content = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json")
+            };
+
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var response = await httpResponseMessage.Content.ReadFromJsonAsync<WalkDto>();
+            if (response is not null)
+            {
+                return RedirectToAction("Index", "Walks");
+            }
+
+            return View();
         }
     }
 }
---
+++ NZWalks/NZWalks.UI/Models/AddWalkViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace NZWalks.UI.Models
+{
+    public class AddWalkViewModel
+    {
+        required public string Name { get; set; }
+        required public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+    }
+}
